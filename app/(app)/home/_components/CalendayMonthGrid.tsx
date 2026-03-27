72ef7006de803c8ba217486e892763d743f40cac
JikMyle <itsmejimkyle@gmail.com>
2026-03-27 15:58:16 +0800

    Fix calendar displaying days from next month

--- app/(app)/home/_components/CalendayMonthGrid.tsx
+++ app/(app)/home/_components/CalendayMonthGrid.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { ReactNode } from "react";
 import { CalendarCell } from "./CalendarCell";
@@ -8,13 +8,16 @@ export function CalendarMonthGrid(): ReactNode {
     const { state, dispatch, data } = useCalendarContext();
 
     // Use UTC-based date math to avoid server-client date mismatch
-    const firstDay = new Date(Date.UTC(state.year, state.month, 1)).getUTCDay();
-    const lastDay = new Date(Date.UTC(state.year, state.month + 1, 0)).getUTCDate();
+    const firstDay = new Date(
+        Date.UTC(state.year, state.month - 1, 1),
+    ).getUTCDay();
 
-    const counts = data?.birthdates[state.month].days || {}
-    
+    const lastDay = new Date(Date.UTC(state.year, state.month, 0)).getUTCDate();
+
+    const counts = data?.birthdates[state.month].days || {};
+
     const cells: ReactNode[] = [];
-    for (let index = (-1 * firstDay) + 1; index < 36 - firstDay; index++) {
+    for (let index = -1 * firstDay + 1; index < 36 - firstDay; index++) {
         if (index < 1 || index > lastDay) {
             cells.push(<CalendarCell key={index} />);
             continue;
@@ -25,13 +28,15 @@ export function CalendarMonthGrid(): ReactNode {
             continue;
         }
 
-        cells.push(<CalendarCell key={index} date={index} count={counts[index + 1]} />);
+        cells.push(
+            <CalendarCell key={index} date={index} count={counts[index + 1]} />,
+        );
     }
 
     return (
         <div className="grid grid-cols-7 grid-rows-[2rem_repeat(5,1fr)] grow text-base-content">
             <CalendarMonthHeaderRow />
-            { cells }
+            {cells}
         </div>
     );
 }
@@ -41,9 +46,11 @@ function CalendarMonthHeaderRow(): ReactNode {
         <>
             {days.map((day, index) => {
                 return (
-                    <div className={`flex justify-center items-center bg-base-100 text-secondary border border-primary text-center
+                    <div
+                        className={`flex justify-center items-center bg-base-100 text-secondary border border-primary text-center
                         ${index !== 0 ? "border-l-0" : ""}`}
-                        key={index}>
+                        key={index}
+                    >
                         <span className="font-bold text-xs">{day}</span>
                     </div>
                 );
