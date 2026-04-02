9b98af3a118afcd0608d566aaf8b83fffd58ed06
JikMyle <itsmejimkyle@gmail.com>
2026-04-02 15:56:42 +0800

    Fix session cookie secure in development environment

--- libs/dal/session.ts
+++ libs/dal/session.ts
@@ -46,7 +46,7 @@ export async function createSession(payload: SessionPayload) {
 
     cookieStore.set("session", session, {
         httpOnly: true,
-        secure: true,
+        secure: process.env.NODE_ENV === "production",
         expires: expiresAt,
         sameSite: "lax",
         path: "/",
@@ -66,7 +66,7 @@ export async function updateSession() {
     const cookieStore = await cookies();
     cookieStore.set("session", session, {
         httpOnly: true,
-        secure: true,
+        secure: process.env.NODE_ENV === "production",
         expires: expires,
         sameSite: "lax",
         path: "/",
