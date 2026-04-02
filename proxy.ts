3da6b55bb60e5013102b64ec037b7f238021e166
JikMyle <itsmejimkyle@gmail.com>
2026-04-02 19:09:47 +0800

    Refactor proxy to redirect signed and non-signed in users from respective pages

--- proxy.ts
+++ proxy.ts
@@ -2,10 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 import { updateSession, verifySession } from "./libs/dal/session";
 import { Role } from "./generated/prisma/enums";
 
-export async function proxy(req: NextRequest) {
-    if (req.nextUrl.pathname.startsWith("/api")) {
-        const session = await verifySession();
+const noAuthRoutes = ["/login", "/signup"];
+const authRoutes = ["/home"];
 
+const stringStartWithAny = function (str: string, subs: string[]) {
+    return subs.some((route) => str.startsWith(route));
+};
+
+export async function proxy(req: NextRequest) {
+    const session = await verifySession();
+
+    if (req.nextUrl.pathname.startsWith("/api")) {
         if (!session.isAuth) {
             return NextResponse.json(
                 { message: "Must be logged in to access" },
@@ -27,8 +34,24 @@ export async function proxy(req: NextRequest) {
             }
         }
     }
+
+    if (
+        stringStartWithAny(req.nextUrl.pathname, noAuthRoutes) &&
+        session.isAuth
+    ) {
+        console.log("Redirect to home");
+        return NextResponse.redirect(new URL("/home", req.url));
+    }
+
+    if (
+        stringStartWithAny(req.nextUrl.pathname, authRoutes) &&
+        !session.isAuth
+    ) {
+        console.log("Redirect to login");
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
 }
 
 export const config = {
-    matcher: ["/api/((?!auth/).*)"],
+    matcher: ["/api/((?!auth/).*)", "/login", "/signup", "/home"],
 };
