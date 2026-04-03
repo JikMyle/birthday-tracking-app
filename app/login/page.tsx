356dbd8045ec5761d8fdc3766d58ae69a15251a5
JikMyle <itsmejimkyle@gmail.com>
2026-04-03 15:02:35 +0800

    Remove session verification call Login page component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,6 @@
-import { verifySession } from "@/libs/dal/session";
 import { SignInForm } from "./SignInForm";
 
 export default async function Login() {
-    const session = await verifySession();
-
     return (
         <main className="grow pt-16 md:pt-32 bg-linear-30 from-white to-primary-content">
             <SignInForm />
