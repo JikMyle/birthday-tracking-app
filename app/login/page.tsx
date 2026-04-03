23da4e26eaaffcb2849a650c56cc68bd6cb6b1b7
JikMyle <itsmejimkyle@gmail.com>
2026-04-03 15:41:15 +0800

    Add FormHeader to Login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,14 @@
+import { AppIcon } from "../_components/AppIcon";
+import { FormHeader } from "../_components/form/FormHeader";
 import { SignInForm } from "./SignInForm";
 
 export default async function Login() {
     return (
-        <main className="grow pt-16 md:pt-32 bg-linear-30 from-white to-primary-content">
+        <main className="flex flex-col items-center grow py-16 md:py-32 bg-linear-30 from-white to-primary-content gap-8">
+            <FormHeader
+                icon={<AppIcon className="text-white bg-primary" />}
+                title="Sign in to BDBashboard"
+            />
             <SignInForm />
         </main>
     );
