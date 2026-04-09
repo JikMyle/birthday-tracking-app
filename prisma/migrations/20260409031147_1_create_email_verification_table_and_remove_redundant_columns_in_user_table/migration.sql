f5452c001500248293c996a6ca4ea51eb75fb87f
JikMyle <itsmejimkyle@gmail.com>
2026-04-09 11:12:17 +0800

    Fix minor prisma bugs and add postcss
    - Fixed prisma config not reading development environment variables
    - Fixed email verification table lacking primary on migration
    - Added postcss dependencies

--- prisma/migrations/20260409031147_1_create_email_verification_table_and_remove_redundant_columns_in_user_table/migration.sql
+++ prisma/migrations/20260409031147_1_create_email_verification_table_and_remove_redundant_columns_in_user_table/migration.sql
@@ -16,7 +16,7 @@ CREATE TABLE `EmailVerification` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `expiresAt` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `EmailVerification_userId_key`(`userId`)
+    PRIMARY KEY (`userId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
