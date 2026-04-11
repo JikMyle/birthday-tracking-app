f004bc45a9772730e825c0b6449fb248d1939180
JikMyle <itsmejimkyle@gmail.com>
2026-04-11 12:44:28 +0800

    Add logging to password encryption module

--- libs/bcrypt.ts
+++ libs/bcrypt.ts
@@ -1,12 +1,31 @@
 import bcrypt from "bcryptjs";
+import logger from "./logger";
 
 const SALT = 10;
 
 export async function encryptPassword(password: string) {
-    return bcrypt.hash(password, SALT);
+    const child = logger.child({
+        function: encryptPassword.name,
+    });
+
+    child.trace({ passwordPresent: !!password }, "Encrypting password");
+    const hashed = bcrypt.hash(password, SALT);
+    child.trace({ hashPresent: !!hashed }, "Password encrypted");
+
+    return hashed;
 }
 
 export async function comparePassword(password: string, hashed: string) {
-    const result = await bcrypt.compare(password, hashed);
-    return result;
+    const child = logger.child({
+        function: comparePassword.name,
+    });
+
+    child.trace(
+        { passwordPresent: !!password, hashPresent: !!hashed },
+        "Comparing passwords",
+    );
+
+    const isMatch = await bcrypt.compare(password, hashed);
+    child.trace(isMatch ? "Passwords matched" : "Passwords did not match");
+    return isMatch;
 }
